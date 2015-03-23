00b6fa6e8a817b75d734c8099d3958ace8a2efb6
Defoy <am01620@logti-a3412-28.local>
2015-03-23 12:49:45 -0400

    Upgrade la formule du Dct pour des bloc autre que 8X8 pour pouvoir tester avec le devoir 2

--- src/gti310/tp4/dct/DCTEncoder.java
+++ src/gti310/tp4/dct/DCTEncoder.java
@@ -9,8 +9,15 @@ public class DCTEncoder {
 		new int[]{200,205,200,200,195,188,187,175},
 		new int[]{200,200,200,200,200,190,187,175},
 		new int[]{205,200,199,200,191,187,187,175},
-		new int[]{210,200,200,200,188,185,187,186},
+		new int[]{210,200,200,200,188,185,187,186}
 	};
+	//Test avec le devoir 2 :)
+	/*
+	int[][] _testArray = new int[][]{
+			new int[]{20,22},
+			new int[]{23,25}
+		};
+	*/
 	int [][] _testOutputArray = new int[][]{
 		new int[]{1539,65,-12,4,1,2,-8,5},
 		new int[]{-16,3,2,0,0,-11,-2,3},
@@ -19,18 +26,18 @@ public class DCTEncoder {
 		new int[]{0,-2,7,-5,4,0,-1,-4},
 		new int[]{0,-3,-1,0,4,1,-1,0},
 		new int[]{3,-2,-3,3,3,-1,-1,3},
-		new int[]{-2,5,-2,4,-2,2,-3,0},
+		new int[]{-2,5,-2,4,-2,2,-3,0}
 	};
 	int [][] _encodedBloc = new int[8][8];
 	public DCTEncoder(){
-		for (int u = 0; u < 8; u++) {
-			for (int v = 0; v < 8; v++) {
+		for (int u = 0; u < _testArray.length; u++) {
+			for (int v = 0; v < _testArray.length; v++) {
 				double firstSomResult = 0;
 				for (int i = 0; i < _testArray.length; i++) {
 					double secondSomResult = 0;
 					for (int j = 0; j < _testArray[i].length; j++) {
-						double cosP = Math.cos((((2*i)+1)*u*Math.PI)/16);
-						double cosD = Math.cos((((2*j)+1)*v*Math.PI)/16);
+						double cosP = Math.cos((((2*i)+1)*u*Math.PI)/(_testArray.length*2));
+						double cosD = Math.cos((((2*j)+1)*v*Math.PI)/(_testArray.length*2));
 						secondSomResult = secondSomResult+((cosP*cosD)*_testArray[i][j]);
 					}
 					firstSomResult = firstSomResult + secondSomResult;
@@ -45,18 +52,13 @@ public class DCTEncoder {
 					cv = 1/Math.sqrt(2);
 				else
 					cv = 1;
-				_encodedBloc[u][v] = (int) (((cu*cv)/4)*firstSomResult);
+				_encodedBloc[u][v] = (int) Math.round((((cu*cv)/(_testArray.length/2))*firstSomResult));
 			}
-			System.out.println(
-				_encodedBloc[u][0] + "," +
-				_encodedBloc[u][1] + "," +
-				_encodedBloc[u][2] + "," +
-				_encodedBloc[u][3] + "," +
-				_encodedBloc[u][4] + "," +
-				_encodedBloc[u][5] + "," +
-				_encodedBloc[u][6] + "," +
-				_encodedBloc[u][7]
-			);
+			String output = "";
+			for (int i = 0; i < _testArray.length; i++) {
+					output += _encodedBloc[u][i] + ",";
+			}
+			System.out.println(output);
 		}
 	}
 }
