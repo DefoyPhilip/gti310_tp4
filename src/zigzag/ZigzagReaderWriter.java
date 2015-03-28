1c2c3c6ade22276c89864863c09be88ba7c032f0
Stéphane Lam <stelam@gmail.com>
2015-03-28 14:39:13 -0400

    test values for zigzag

---
+++ src/zigzag/ZigzagReaderWriter.java
@@ -0,0 +1,37 @@
+package zigzag;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ZigzagReaderWriter {
+	
+	private int[][] _testQuantified = new int[][]{
+		new int[]{96,6,-1,0,0,0,0,0},
+		new int[]{-1,0,0,0,0,0,0,0},
+		new int[]{-1,0,1,0,0,0,0,0},
+		new int[]{-1,0,0,0,0,0,0,0},
+		new int[]{0,0,0,0,0,0,0,0},
+		new int[]{0,0,0,0,0,0,0,0},
+		new int[]{0,0,0,0,0,0,0,0},
+		new int[]{0,0,0,0,0,0,0,0}
+	};
+	
+	List<Integer> strings = Arrays.asList(96,6,-1,-1,0,-1,0,0,0,
+			1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+			0,0,0);
+	
+	
+	public ZigzagReaderWriter() {
+		
+	}
+	
+	public void write() {
+		
+	}
+	
+	public void read() {
+		
+	}
+}
